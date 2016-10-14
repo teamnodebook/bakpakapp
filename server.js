89ea4a05f277466e6aea67f2a7fda09805c20b3c
Jason <jmaier@berkeley.edu>
2016-10-14 11:46:55 -0700

    slash

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-var publicPath = path.resolve(__dirname);
+var publicPath = path.resolve(__dirname, '/');
 
 // We point to our static assets
 app.use(express.static(publicPath));
