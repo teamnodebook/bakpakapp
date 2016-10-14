a5e3e016aa673ac1704f7ec9942bac0dc3d65625
Jason <jmaier@berkeley.edu>
2016-10-14 13:56:19 -0700

    test nav render

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app.jsx';
+import Nav from './nav.jsx';
 
 render(<App/>, document.querySelector("#app"));
+render(<Nav/>, document.querySelector("#nav"));
