5311bc93a8de0b39855b71b73c006f43a5a649b7
Jason <jmaier@berkeley.edu>
2016-10-14 11:26:25 -0700

    Node Server

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+var publicPath = path.resolve(__dirname, 'public');
+
+// We point to our static assets
+app.use(express.static(publicPath));
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+	console.log("App listening on port: ", port);
+});
