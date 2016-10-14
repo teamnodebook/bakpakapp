25031c0769b1bfa9b133d487b27371c6312e5f52
Jason <jmaier@berkeley.edu>
2016-10-14 11:03:33 -0700

    initial commit

--- src/app.jsx
+++ src/app.jsx
@@ -6,9 +6,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className={styles.blueBg}>module</span> local styles.</p>
-        <p>Global bootstrap css import works too as you can see on the following button.</p>
+        <h1>Hello World Again!</h1>
         <p><a className="btn btn-primary btn-lg">Enjoy!</a></p>
       </div>
     )
